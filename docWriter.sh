45034fe4f85f3d0020610bd0e8b12f9c5ad5f3dd
Boris Moers <boris@moers.ch>
2024-08-29 14:08:30 +0200

    Update docWriter.sh to include additional command line options

--- docWriter.sh
+++ docWriter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mvn package
-java -jar ./target/*.jar $@
+java -jar ./target/*.jar --srcDir=$(pwd)/src/test/java --logLevel=debug --maxFileToChange=2 --publicMethodDoc=true --nonPublicMethodDoc=true $@
